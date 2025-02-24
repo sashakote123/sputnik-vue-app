b765b880c3743f66781f5646ac676dae10617217
Kotikhin Alex <sapool@bk.ru>
2025-02-24 23:00:44 +0300

    add some components and filter

---
+++ src/components/EventCard.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="container">
+        <img :src="img" alt="123">
+        <div>{{ name }}</div>
+        <div>{{ score }}</div>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: 'EventCard',
+    props: {
+        img: {
+            type: String,
+            required: true
+        },
+        score: {
+            type: Number,
+        },
+        name: {
+            type: String
+        },
+        price: {
+            type: Number
+        }
+    }
+};
+</script>
+
+<style>
+.container {
+    border: 1px, red, solid;
+    margin: 5px;
+}
+</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,26 +6,63 @@
     <h1 class="title">Экскурсии по всему миру</h1>
     <form action="">
       <input type="text">
-      <select name="" id="">
-        <option value="">123</option>
-        <option value="">123</option>
+      <select v-model="selectedCityId" @change="handleCityChange">
+        <option value="" disabled>Выберите город</option>
+        <option v-for="city in cities" :key="city.id" :value="city.id">
+          {{ city.name }}
+        </option>
       </select>
+      <div v-for="event in eventsArray" :key="event.id">
+        <EventCard :img="event.cover_photo.original" :score="event.customers_review_rating" :name="event.title"
+          :price="event.netto_price" />
+      </div>
     </form>
+
   </div>
 </template>
 
 <script>
+import EventCard from './EventCard.vue';
+
+
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
   },
+  components: {
+    EventCard
+  },
+  data() {
+    return {
+      cities: [],
+      products: [],
+      selectedCityId: '', // ID выбранного города
+      eventsArray: [],
+    }
+  },
+  methods: {
+    handleCityChange() {
+      const selectedCity = this.cities.find(city => city.id === this.selectedCityId);
+      console.log('Выбранный город:', selectedCity, selectedCity.region_id);
+      //console.log(this.products);
+      const arr = this.products.filter(product => product.city_id === selectedCity.region_id)
+      this.eventsArray = arr
+      console.log(arr);
+    }
+  },
   mounted() {
     fetch('https://cors-anywhere.herokuapp.com/https://api.sputnik8.com/v1/products?api_key=873fa71c061b0c36d9ad7e47ec3635d9&username=frontend@sputnik8.com')
       .then(response => response.json())
-      .then(data => console.log(data))
+      .then(data => this.products = data)
       .catch(error => console.error(error));
-  }
+    fetch('https://cors-anywhere.herokuapp.com/https://api.sputnik8.com/v1/cities?api_key=873fa71c061b0c36d9ad7e47ec3635d9&username=frontend@sputnik8.com')
+      .then(response => response.json())
+      .then(data => this.cities = data)
+      .catch(error => console.error(error));
+  },
+
 }
 </script>
 
